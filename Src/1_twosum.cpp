32f7fa70f90817f99ec9a942f261b3be06acc842
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-02-12 00:30:54 -0800

    prob1

--- Src/1_twosum.cpp
+++ Src/1_twosum.cpp
@@ -1,16 +1,31 @@
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
 vector<int> twoSum(vector<int>& nums, int target) {
 
-    using SizeType = remove_reference_t<decltype(nums)>::size_type;
-    using ValueToIndexMapType = unordered_map<int, SizeType>;
-    ValueToIndexMapType map;
-    vector<int> indices(2);
-    for (SizeType index = 0; index < nums.size(); ++index)
-    {
-        const auto foundIterator = map.find(target - nums[index]);
-        if (foundIterator != end(map) && foundIterator->second != index )
-            return vector<int>{ index, foundIterator->second };
-        else
-            map.emplace(nums[index], index);
-    }
-    throw std::runtime_error("Solution not found");
+	using SizeType = int;
+	using ValueToIndexMapType = unordered_map<int, SizeType>;
+	ValueToIndexMapType map;
+	vector<int> indices(2);
+	for (SizeType index = 0; index < nums.size(); ++index)
+	{
+		const auto foundIterator = map.find(target - nums[index]);
+		if (foundIterator != end(map) && foundIterator->second != index)
+			return vector<int>{ index, foundIterator->second };
+		else
+			map.emplace(nums[index], index);
+	}
+	throw std::runtime_error("Solution not found");
+}
+
+int main()
+{
+
+	vector<int> nums = { 3,2,4 };
+	vector<int> ret = twoSum(nums, 6);
+	return 0;
 }
