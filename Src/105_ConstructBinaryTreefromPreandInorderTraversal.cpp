f1f3c768a9be6fefb8f7a55b839f17955bd9f14e
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-02-14 20:49:15 -0800

    Iterative solution?

---
+++ Src/105_ConstructBinaryTreefromPreandInorderTraversal.cpp
@@ -0,0 +1,62 @@
+/* Given preorder and inorder traversal of a tree, construct the binary tree.
+*
+* Note:
+* You may assume that duplicates do not exist in the tree.
+*
+* For example, given
+*
+* preorder = [3,9,20,15,7]
+* inorder = [9,3,15,20,7]
+* Return the following binary tree:
+*
+*     3
+*    / \
+*   9  20
+*     /  \
+*    15   7
+*
+*/
+#include <vector>
+
+using namespace std;
+
+/*
+* Definition for a binary tree node.
+*/
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* _buildTree(const vector<int>& preorder, int& i, const vector<int>& inorder, int ibeg, int iend) {
+
+	/* to build tree from pre-Order array, recursion should use pre-Order fashion */
+	while (ibeg <= iend) {
+		int imid = find(inorder.begin()+ibeg, inorder.begin()+iend, preorder[i]) - inorder.begin();
+		TreeNode * pNode = new TreeNode(preorder[i++]);
+		TreeNode * lChild = _buildTree(preorder, i, inorder, ibeg, imid - 1);
+		TreeNode * rChild = _buildTree(preorder, i, inorder, imid + 1, iend);
+		pNode->left = lChild;
+		pNode->right = rChild;
+		return pNode;
+	}
+	return nullptr;
+
+}
+
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+	int N = preorder.size();
+	int i = 0;
+	return _buildTree(preorder, i, inorder, 0, N - 1);
+}
+
+int main()
+{
+	vector<int> preorder = { 3,9,20,15,7 };
+	vector<int> inorder = { 9,3,15,20,7 };
+
+	TreeNode* ret1 = buildTree(preorder, inorder);
+	return 0;
+}
