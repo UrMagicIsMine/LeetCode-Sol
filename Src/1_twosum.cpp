68eea5a1db505bbfca4525827aed1fa2f593132f
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-04-13 20:16:32 -0700

    hashtable

--- Src/1_twosum.cpp
+++ Src/1_twosum.cpp
@@ -10,11 +10,10 @@ vector<int> twoSum(vector<int>& nums, int target) {
 	using SizeType = int;
 	using ValueToIndexMapType = unordered_map<int, SizeType>;
 	ValueToIndexMapType map;
-	vector<int> indices(2);
 	for (SizeType index = 0; index < nums.size(); ++index)
 	{
 		const auto foundIterator = map.find(target - nums[index]);
-		if (foundIterator != end(map) && foundIterator->second != index)
+		if (foundIterator != end(map))
 			return vector<int>{ index, foundIterator->second };
 		else
 			map.emplace(nums[index], index);
