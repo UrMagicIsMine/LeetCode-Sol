48e234a9b1a8771cbe24db8c90611184b21b1b01
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-04-13 22:37:45 -0700

    hashtable and array sort

--- Src/1_twosum.cpp
+++ Src/1_twosum.cpp
@@ -1,30 +1,81 @@
+/* Given an array of integers, return indices of the two numbers such that they
+* add up to a specific target.
+*
+* You may assume that each input would have exactly one solution, and you may
+* not use the same element twice.
+*
+* Example:
+*
+* Given nums = [2, 7, 11, 15], target = 9,
+*
+* Because nums[0] + nums[1] = 2 + 7 = 9,
+* return [0, 1].
+*/
+
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
-#include <map>
-
+#include <cassert>
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target) {
+/*While we iterate and inserting elements into the table, we also look back to
+* check if current element's complement already exists in the table. If it exists,
+* we have found a solution and return immediately.
+* run-time O(N), memory O(N)
+*/
 
-	using SizeType = int;
-	using ValueToIndexMapType = unordered_map<int, SizeType>;
-	ValueToIndexMapType map;
-	for (SizeType index = 0; index < nums.size(); ++index)
-	{
-		const auto foundIterator = map.find(target - nums[index]);
-		if (foundIterator != end(map))
-			return vector<int>{ index, foundIterator->second };
+vector<int> twoSum_HT(vector<int>& nums, int target) {
+
+	unordered_map<int, int> hashtbl;
+	for (int i = 0; i < nums.size(); i++) {
+		auto it = hashtbl.find(target - nums[i]);
+		if (it != hashtbl.end())
+			return vector<int>{it->second, i};
 		else
-			map.emplace(nums[index], index);
+			hashtbl.emplace(nums[i], i);
 	}
-	throw std::runtime_error("Solution not found");
+	return vector<int>{0, 0};
+}
+
+/* sort the original array, use two pointer to search for the sum target,
+* run-time O(NlogN), memory O(N)
+*/
+
+vector<int> twoSum_ST(vector<int>& nums, int target) {
+
+	vector<int> num_copy(nums.size());
+	copy(nums.begin(), nums.end(), num_copy.begin());
+	sort(num_copy.begin(), num_copy.end());
+	int low = 0, high = (int)nums.size() - 1;
+	vector<int> targetnum(2);
+	while (low < high) {
+		if (num_copy[low] + num_copy[high] == target) {
+			targetnum[0] = num_copy[low];
+			targetnum[1] = num_copy[high];
+			break;
+		}
+		else if (num_copy[low] + num_copy[high] < target)
+			low++;
+		else
+			high--;
+	}
+
+	vector<int> resl;
+	for (int i = 0; i < nums.size(); i++) {
+		if (nums[i] == targetnum[0] || nums[i] == targetnum[1])
+			resl.push_back(i);
+	}
+	return resl;
 }
 
 int main()
 {
-
-	vector<int> nums = { 3,2,4 };
-	vector<int> ret = twoSum(nums, 6);
+	vector<int> nums = { 2, 7, 11, 15 };
+	int target = 9;
+	vector<int> ans = { 0,1 };
+	vector<int> ret1 = twoSum_HT(nums, target);
+	assert(ret1 == ans);
+	vector<int> ret2 = twoSum_ST(nums, target);
+	assert(ret2 == ans);
 	return 0;
 }
