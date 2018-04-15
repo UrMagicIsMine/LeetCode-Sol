64172156e563a81a75c2c09b456e6c8bcab6ac06
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-04-15 14:23:14 -0700

    Brute-force comparison

---
+++ Src/14_LongestCommonPrefix.cpp
@@ -0,0 +1,41 @@
+/* Write a function to find the longest common prefix string amongst an array
+* of strings.
+*/
+
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+
+	int N = INT_MAX;
+	if (strs.size() == 0)
+		return string("");
+
+	if (strs.size() == 1)
+		return strs[0];
+
+	for (int i = 0; i < strs.size(); i++) {
+		N = min<int>(strs[i].size(), N);
+	}
+
+	for (int j = 0; j < N; j++) {
+		for (int i = 1; i < strs.size(); i++) {
+			if (strs[0][j] != strs[i][j]) {
+				return strs[0].substr(0, j);
+			}
+		}
+	}
+	return strs[0].substr(0, N);
+}
+
+int main()
+{
+	vector<string> strs = { "Hello World", "Hello" };
+	string ans = "Hello";
+	string ret = longestCommonPrefix(strs);
+	assert(ret == ans);
+	return 0;
+}
