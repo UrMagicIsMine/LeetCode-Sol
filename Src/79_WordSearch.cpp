de794af1be859c45a373e75a529f0487bf10ea3c
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-04-28 17:36:42 -0700

    DFS search solution

---
+++ Src/79_WordSearch.cpp
@@ -0,0 +1,78 @@
+/* Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where
+"adjacent" cells are those horizontally or vertically neighboring. The same
+letter cell may not be used more than once.
+
+Example:
+
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+
+Given word = "ABCCED", return true.
+Given word = "SEE", return true.
+Given word = "ABCB", return false.
+*/
+#include <vector>
+#include <cassert>
+using namespace std;
+
+bool _DFS(vector<vector<char>>&board, int M, int N, int i, int j, const string& word, int pos) {
+
+	if (pos == word.size())
+		return true;
+
+	if (i < 0 || i >= M || j < 0 || j >= N)
+		return false;
+
+	if (board[i][j] == word[pos]) {
+		board[i][j] = '#';
+		bool ret = false;
+		ret = _DFS(board, M, N, i - 1, j, word, pos + 1);
+		if (ret) return true;
+		ret = _DFS(board, M, N, i + 1, j, word, pos + 1);
+		if (ret) return true;
+		ret = _DFS(board, M, N, i, j - 1, word, pos + 1);
+		if (ret) return true;
+		ret = _DFS(board, M, N, i, j + 1, word, pos + 1);
+		if (ret) return true;
+		board[i][j] = word[pos];
+	}
+	return false;
+}
+
+bool exist(vector<vector<char>>& board, string word) {
+	int Len = word.length();
+	if (Len == 0)
+		return false;
+
+	int M = board.size();
+	if (M == 0)
+		return false;
+	int N = board[0].size();
+	if (N == 0)
+		return false;
+
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			int ret = _DFS(board, M, N, i, j, word, 0);
+			if (ret == true)
+				return true;
+		}
+	}
+	return false;
+}
+
+int main()
+{
+	vector<vector<char>> board = { { 'A','B','C','E' },{'S','F','C','S'},{'A','D','E','E'} };
+	string word = "ABCCED";
+	bool ans = true;
+	bool ret = exist(board, word);
+	assert(ret == ans);
+	return 0;
+}
