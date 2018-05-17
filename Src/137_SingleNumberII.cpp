52ec3a637eea5bd81708788911821141173b595c
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-05-16 23:51:20 -0700

    Boolen logic

--- Src/137_SingleNumberII.cpp
+++ Src/137_SingleNumberII.cpp
@@ -12,14 +12,29 @@
 using namespace std;
 
 /*
-*  need further analysis
+*  Boolean logic. Consider a single bit input and how that would drive the internal
+state of a bit counter in the state table below. An input value '0' does not change
+the internal state. Input of '1' drives the counter from state 0, 1, 2 and resets
+back to zero.
+
+The state table can be implemented as a boolean circuit with two XOR, two AND,
+and two NOT gates. That construction is reflected in the solution code. Other
+constructions with identical functionality are also possible.
+
+'A'  'twos'  'ones'    'twos'  'ones'
+ 0      0       0    |    0       0
+ 0      0       1    |    0       1
+ 0      1       0    |    1       0
+ 1      0       0    |    0       1
+ 1      0       1    |    1       0
+ 1      1       0    |    0       0
+
 */
 
 int singleNumber(vector<int>& nums) {
 
 	int ones = 0, twos = 0;
 	for (int i = 0; i < nums.size(); i++) {
-		int tmp = ones ^ nums[i];
 		ones = (ones ^ nums[i]) & ~twos;
 		twos = (twos ^ nums[i]) & ~ones;
 	}
