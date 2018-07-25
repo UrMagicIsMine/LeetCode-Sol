affbf4ac418f56ffb0cae153202694b280c7a5e8
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-07-25 13:57:27 -0700

    Updated solution

--- Src/316_RemoveDuplicateLetters.cpp
+++ Src/316_RemoveDuplicateLetters.cpp
@@ -16,49 +16,26 @@
 #include <cassert>
 using namespace std;
 
-/*
-Given the string s, the greedy choice (i.e., the leftmost letter in the answer)
-is the smallest s[i] such that the suffix s[i … ] contains all the unique letters
-(Note that, when there are more than one smallest s[i]'s, we choose the leftmost one.
-Why? Simply consider the example: “abcacb”.)
-
-After determining the greedy choice s[i], we get a new string s’ from s by
-  removing all letters to the left of s[i],
-  removing all s[i]'s from s.
-We then recursively solve the problem w.r.t.s’.
-
-The runtime is O(26 * n) = O(n).
-*/
-string _remove(string& str, char c) {
-	string ret;
-	for (auto t : str) {
-		if (t != c)
-			ret.push_back(t);
-	}
-	return ret;
-}
-
 string removeDuplicateLetters(string s) {
-	vector<int> cnt(26,0);
-	int pos = 0;
+
+	vector<int> freq(256, 0);
 	for (auto c : s)
-		cnt[c - 'a']++;
+		freq[c]++;
 
-	for (int i = 0; i < s.length(); i++) {
-		if(s[i] < s[pos])
-			pos = i;
-		if (--cnt[s[i] - 'a'] == 0)
-			break;
+	string ret = "0";
+	vector<bool> visited(256, false);
+
+	for (auto c : s) {
+		freq[c]--;
+		if (visited[c]) continue;
+		while (c < ret.back() && freq[ret.back()]) {
+			visited[ret.back()] = false;
+			ret.pop_back();
+		}
+		ret.push_back(c);
+		visited[c] = true;
 	}
-
-	if (s.length() == 0)
-		return "";
-	else {
-		string str = _remove(s.substr(pos + 1), s[pos]);
-		string tmp = removeDuplicateLetters(str);
-		return s[pos] + tmp;
-	}
-
+	return ret.substr(1);
 }
 
 int main()
