682b9d707bbb082b7d5132c10c4c74dfdf459856
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-07-26 20:44:38 -0700

    neat solution update

--- Src/290_WordPattern.cpp
+++ Src/290_WordPattern.cpp
@@ -91,6 +91,21 @@ bool wordPattern_Sln2(string pattern, string str) {
 	return true;
 }
 
+bool wordPattern(string pattern, string str) {
+	unordered_map<char, int> p2i;
+	unordered_map<string, int> w2i;
+	istringstream in(str);
+	int i = 0, n = pattern.size();
+	string word;
+	while (in >> word) {
+		if (i == n || p2i[pattern[i]] != w2i[word])
+			return false;
+		p2i[pattern[i]] = w2i[word] = i + 1;
+		i++;
+	}
+	return i == n;
+}
+
 int main()
 {
 	string pattern = "abba", str = "dog cat cat dog";
