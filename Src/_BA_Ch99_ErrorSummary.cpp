d7159a32809ffeee912554b9cbc128e2f6a6cf10
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-08-02 10:48:44 -0700

    update error list

--- Src/_BA_Ch99_ErrorSummary.cpp
+++ Src/_BA_Ch99_ErrorSummary.cpp
@@ -1,7 +1,9 @@
 // Prob2: remember to reset carry;
 // Prob3: remember to compare whole length;
+// Prob4: [Important] follow up O(log(M+N)) array
 // Prob5: loop the whole length;
 // Prob8: be careful about leading 0;
+// Prob10: [Important] Spent more time!!!, be careful about case ".*"
 // Prob11: Don't forget to use max;
 // Prob15: Be careful about the move of idx after find match;
 // Prob23: Do not push nullptr to heap;
@@ -9,12 +11,16 @@
 // Prob29: 1) all value use long long; 2) Don't forget equal cases;
 // Prob30: Be careful about <,<= and >, >=
 // Prob38: Be careful about i++/string +=
+// Prob41: continue swap, 1) Be careful about index, 2) be careful about [1,1]
+// Prob44: [Important] Spent more time!!!, analysis IC, DP table
 // Prob54: Be careful about switch direction
 // Prob62: Do not overflow array
 // Prob70: Do not overflow dp array, allocate (N+1) size
 // Prob75: Do not need to care what is in nums[low] and nums[high], regardless
+// Prob76: [Important] Spent more time!!! Two pointer + hash map
 // of whether it's the desired value in this run; take care when nums[i] == 2,
 // after swap, nums[i] could be any value, so i--
+// Prob84: [Important] Spent more time!!! use stack to maintain index
 // Prob91: leading zero, be careful about 02, 10, 20, 27, corner case
 // Prob98: INT over flow, LLONG_MIN, LLONG_MAX
 // Prob101: be careful about the comparison of two pointer
@@ -22,6 +28,7 @@
 // Prob127: use hash table and two end search to optimize
 // Prob130: be careful about "=" and "==", index of 2d array
 // Prob138: be careful about the case where random point to nullptr
+// Prob140: [Important] backtracking, TLE.
 // Prob148: [Important] Spent more time!!!
 // Prob139: [Important] Spent more time!!!
 // Prob152: [Important] Spent more time!!!
@@ -39,6 +46,7 @@
 // prob279: use BFS, visit flag, consider break loop when nextsum > target
 // prob300: use lower_bound to build a LIS array
 // prob322: DP solution
+// prob324: [Important] sort and reorder, from back to front, use nth_element
 // prob334: use two number to maintain the two min values
 // prob341:
 // prob347: hash table and bucket sort
