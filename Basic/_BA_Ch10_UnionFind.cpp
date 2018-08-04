baae4693ce0eb77f287a3e2c1addfdb753e6294b
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-08-04 10:39:00 -0700

    Update union find set

--- Basic/_BA_Ch10_UnionFind.cpp
+++ Basic/_BA_Ch10_UnionFind.cpp
@@ -2,37 +2,36 @@
 In this chapter, we discuss the problem of Disjoint Set (Or Union-Find)
 */
 
-void init(vector<int>& par, vector<int>& rank){
-  assert(par.size() == rank.size());
-  for(int i = 0; i < par.size(); i++){
-    par[i] = i;
-    rank[i] = 0;
+class UnionFindSet{
+
+public:
+  UnionFindSet(int n) : _par(n, 0), _rank(n, 0){
+
   }
-}
 
-void find(int x, vector<int>& par){
-  if(x == par[x])
-    return x;
-  else
-    return par[x] = find(par[x], par); // find parrent and compression
-}
-
-void same(int x, int y, vector<int>& par){
-  return find(x, par) == find(y, par);
-}
-
-void union(int x, int y, vector<int>& par, vector<int>& rank){
-  x = find(x, par);
-  y = find(y, par);
-  if(x == y)
-    return;
-
-  if(rank[x] < rank[y])
-    par[x] = par[y];
-  else{
-    par[y] = par[x];
-    if(rank[x] == rank[y])
-      rank[x]++;
+  int find(int x){
+    if (x == _par[x])
+      return x;
+    else
+      return _par[x] = find(_par[x]); // path compression
   }
-  return;
-}
+
+  bool Union(int x, int y){
+    int rx = find(x);
+    int ry = find(y);
+    if(rx == ry) return false; // in the same set already
+
+    if(_rank[rx] < _rank[ry])
+      _par[rx] = ry;
+    else{
+      _par[ry] = rx;
+      if(_rank[rx] == _rank[ry])
+        _rank[rx]++;
+    }
+    return true;
+  }
+
+private:
+  vector<int> _par;
+  vector<int> _rank;
+};
