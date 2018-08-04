001ab8575f5a273895ba278eb01afc20103ff25f
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-08-04 15:34:53 -0700

    Trie

---
+++ Basic/_BA_Ch12_Trie.cpp
@@ -0,0 +1,66 @@
+/*
+In this chapter, we discuss the problem of Trie or Prefix tree
+*/
+
+class Trie{
+
+public:
+
+  Trie() : _root(new TrieNode){
+
+  }
+
+  bool search(const string & str){
+    TrieNode * p = find(str);
+    return p && p->isWord;
+  }
+
+  bool startsWith(const string& str){
+    TrieNode *p = find(str);
+    return p != nullptr;
+  }
+
+  void insert(const string& str){
+    TrieNode *p = _root.get();
+    for(auto c: str){
+      if(p->children[c - 'a'] == nullptr)
+        p->children[c - 'a'] = new TrieNode;
+      p = p->children[c - 'a'];
+    }
+    p->isWord = true;
+    return;
+  }
+
+private:
+
+  struct TrieNode{
+
+    TrieNode() : isWord(false), children(26, nullptr){
+    }
+
+    ~TrieNode(){
+      for(auto p : children)
+        delete p;
+    }
+
+    bool isWord;
+    vector<TrieNode*> children;
+  };
+
+  TrieNode * find(const string & str){
+    TrieNode* p = _root.get();
+
+    for(auto c : str){
+      p = p->children[c-'a'];
+      if(p == nullptr)
+        break;
+    }
+
+    return p;
+  }
+
+private:
+
+  unique_ptr<TrieNode> _root;
+
+};
