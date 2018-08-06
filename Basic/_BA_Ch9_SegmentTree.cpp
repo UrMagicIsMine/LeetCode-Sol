cf6c9605fe7e812f76fa9c50ae61ead56da54693
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-08-05 20:09:12 -0700

    Segment tree

---
+++ Basic/_BA_Ch9_SegmentTree.cpp
@@ -0,0 +1,56 @@
+/*
+In this chapter, we discuss the problem of Segment Tree: A data structure which
+can efficiently answer dynamic range queries.
+*/
+
+class SegmentTree {
+
+public:
+
+	SegmentTree(int n) {
+		_n = 1;
+		while (_n < n) {
+			_n *= 2;
+		}
+
+		_num.assign(2 * _n - 1, INT_MAX);
+	}
+
+	// update k-th (0-indexed) value to val;
+	void update(int k, int val) {
+		k += _n - 1;
+		_num[k] = val;
+
+		while (k > 0) {
+			k = (k - 1) / 2; // get parent
+			_num[k] = min(_num[k * 2 + 1], _num[k * 2 + 2]);
+		}
+		return;
+	}
+
+	// get the minimal of [l, r)
+	int query(int l, int r) {
+		return query(l, r, 0, 0, _n);
+	}
+
+private:
+
+	// get the minimal of [a, b)
+	int query(int a, int b, int k, int l, int r) {
+		// if [a, b) and [l, r) have no intersection
+		if (r <= a || b <= l) return INT_MAX;
+
+		// if [a, b) contains [l, r)
+		if (a <= l && r <= b) return _num[k];
+
+		int vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+		int vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+		return min(vl, vr);
+	}
+
+private:
+
+	vector<int> _num;
+	int _n;
+
+};
