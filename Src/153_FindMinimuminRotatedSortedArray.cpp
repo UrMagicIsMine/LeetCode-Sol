b8d89f08e3b82cd369ee56db50d0f229975cde1b
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2018-09-01 22:16:56 -0700

    update solution

--- Src/153_FindMinimuminRotatedSortedArray.cpp
+++ Src/153_FindMinimuminRotatedSortedArray.cpp
@@ -13,66 +13,34 @@
 #include <algorithm>
 using namespace std;
 
-int findMin_Sol1(vector<int>& nums) {
-
-	int N = nums.size();
-	if (N < 1)
+int findMin(vector<int>& nums) {
+	int n = nums.size();
+	if (n == 0)
 		return INT_MIN;
 
-	/* keep a minum value during serach */
-	int minValue = nums[0];
-	int beg = 0;
-	int end = N - 1;
-
-	while (beg <= end) {
-
-		int mid = (beg + end) / 2;
-		if (nums[mid] < minValue)
-			minValue = nums[mid];
-		/* if mid value is large then the right value,
-		* then the min value must be on the right, else if
-		* may be the mid or on the left;
-		* because mid is already checked, we can just search left;
-		*/
-		if (nums[mid] > nums[end])
-			beg = mid + 1;
-		else
-			end = mid - 1;
-	}
-	return minValue;
-}
-
-int findMin_Sol2(vector<int>& nums) {
-
-	int N = nums.size();
-	if (N < 1)
-		return INT_MIN;
-
-	int beg = 0;
-	int end = N - 1;
-
-	while (beg < end) {
-
-		int mid = (beg + end) / 2;
+	int low = 0, high = n - 1;
+	int mid;
+	while (low < high) {
+		mid = low + (high - low) / 2;
 		/* if mid value is large then the right value,
 		* then the min value must be on the right, else if
 		* may be the mid or on the left;
 		*/
-		if (nums[mid] > nums[end])
-			beg = mid + 1;
-		else
-			end = mid;
+		if (nums[mid] > nums[high]) {
+			low = mid + 1;
+		}
+		else {
+			high = mid;
+		}
 	}
-	return min(nums[beg], nums[end]);
+	return nums[low];
 }
+
 
 int main()
 {
 	vector<int> nums = { 3,4,5,6,7,1,2,3 };
-	int ret1 = findMin_Sol1(nums);
-	assert(ret1 == 1);
-
-	int ret2 = findMin_Sol2(nums);
+	int ret1 = findMin(nums);
 	assert(ret1 == 1);
 	return 0;
 }
