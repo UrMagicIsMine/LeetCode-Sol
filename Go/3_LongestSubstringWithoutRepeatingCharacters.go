e1cf352691bffa2bac086bbfe1556224456bd0b6
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2020-08-02 18:10:57 -0700

    Update 3_LongestSubstringWithoutRepeatingCharacters.go

--- Go/3_LongestSubstringWithoutRepeatingCharacters.go
+++ Go/3_LongestSubstringWithoutRepeatingCharacters.go
@@ -10,3 +10,26 @@
 * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer
 * must be a substring, "pwke" is a subsequence and not a substring.
 */
+
+func max(x, y int) int {
+    if x < y {
+        return y
+    }
+    return x
+}
+
+func lengthOfLongestSubstring(s string) int {
+    ret := 0;
+    back := 0;
+    char_table := make(map[rune]int);
+    for front, c_val := range s {
+        val, stat := char_table[c_val];
+        if (stat == false || val < back) {
+            ret = max(ret, front - back + 1);
+        } else {
+            back = val + 1;
+        }
+        char_table[c_val] = front;
+    }
+    return ret;
+}
