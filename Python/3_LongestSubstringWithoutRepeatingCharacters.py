9032ec36d48c2fa3081465c141fcd8d4178ee8c1
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2020-08-02 17:50:07 -0700

    Update 3_LongestSubstringWithoutRepeatingCharacters.py

--- Python/3_LongestSubstringWithoutRepeatingCharacters.py
+++ Python/3_LongestSubstringWithoutRepeatingCharacters.py
@@ -10,3 +10,20 @@
 # Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer
 # must be a substring, "pwke" is a subsequence and not a substring.
 #
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_table = {}
+        back = 0
+        ret = 0
+        for front in range(len(s)):
+            if (s[front] not in char_table) or (char_table[s[front]] < back):
+                ret = max(ret, front - back + 1)
+            else:
+                back = char_table[s[front]] + 1
+            char_table[s[front]] = front
+        return ret
