2fcf1a1383355467635e186ebf4a58e348885dde
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2020-08-03 21:31:17 -0700

    Update 1_TwoSum.py

--- Python/1_TwoSum.py
+++ Python/1_TwoSum.py
@@ -10,3 +10,18 @@
 #
 # Because nums[0] + nums[1] = 2 + 7 = 9,
 # return [0, 1].
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_table = {}
+        for idx1 in range(len(nums)):
+            num = target - nums[idx1]
+            if num in nums_table:
+                return [nums_table[num], idx1]
+            nums_table[nums[idx1]] = idx1
+        return [-1,-1]
