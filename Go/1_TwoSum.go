8767c59e7b6785bc8191ac70c7a98f9d40440fac
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2020-08-03 21:26:57 -0700

    Update 1_TwoSum.go

--- Go/1_TwoSum.go
+++ Go/1_TwoSum.go
@@ -11,3 +11,14 @@
 * Because nums[0] + nums[1] = 2 + 7 = 9,
 * return [0, 1].
 */
+
+func twoSum(nums []int, target int) []int {
+    num_table := make(map[int]int);
+    for idx1, val := range nums {
+        if idx2, ok := num_table[target - val]; ok {
+            return []int{idx2, idx1};
+        }
+        num_table[val] = idx1;
+    }
+    return []int{-1, -1};
+}
