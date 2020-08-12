b720e78f3969c18349aa83e2f639d32dd2fb26f4
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2020-08-11 22:18:43 -0700

    Update 2_AddTwoNumbers.go

--- Go/2_AddTwoNumbers.go
+++ Go/2_AddTwoNumbers.go
@@ -11,3 +11,32 @@
 * Output: 7 -> 0 -> 8
 * Explanation: 342 + 465 = 807.
 */
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    var dummy = ListNode{0, nil}
+    temp := &dummy
+    carry := 0
+    for l1 != nil || l2 != nil || carry != 0 {
+        value := carry
+        if l1 != nil {
+            value += l1.Val
+            l1 = l1.Next
+        }
+        if l2 != nil {
+            value += l2.Val
+            l2 = l2.Next
+        }
+        carry = value / 10
+        temp.Next = new(ListNode)
+        temp = temp.Next
+        temp.Val = value % 10
+    }
+    return dummy.Next
+}
