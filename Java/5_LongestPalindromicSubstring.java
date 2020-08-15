88c64ed96c406e00e6b3f0fb413134eb540530ff
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2020-08-15 10:53:13 -0700

    Update 5_LongestPalindromicSubstring.java

--- Java/5_LongestPalindromicSubstring.java
+++ Java/5_LongestPalindromicSubstring.java
@@ -13,42 +13,25 @@
 */
 
 public String longestPalindrome(String s) {
-    if ("".equals(s))
-        return "";
-    int max_len = 0, idx_mid = 0;
-    int len = s.length();
-    for (int idx = 0; idx < len; idx++) {
+    if (s == null || s.length() < 1) return "";
+    int left = 0, right =0;
+    for (int idx = 0; idx < s.length(); idx++) {
         int pal_len1 = lengthOfPalindrome(s, idx, idx);
         int pal_Len2 = lengthOfPalindrome(s, idx, idx+1);
-        if (pal_len1 > max_len || pal_Len2 > max_len) {
-            max_len = Math.max(pal_len1, pal_Len2);
-            idx_mid = idx;
+        int len = Math.max(pal_len1, pal_Len2);
+        if (len > right - left + 1) {
+            left = idx - (len - 1) / 2;
+            right = idx + len / 2;
         }
     }
-    int left = 0, right =0;
-    if ((max_len & 1) != 0) {
-        // Odd
-        left = idx_mid - (max_len >> 1);
-        right = idx_mid + (max_len >> 1) + 1;
-    } else {
-        // Even
-        left = idx_mid - (max_len >> 1) + 1;
-        right = idx_mid + (max_len >> 1) + 1;
-    }
-    return s.substring(left, right);
+    return s.substring(left, right+1);
 }
 
 private int lengthOfPalindrome(String s, int i, int j) {
     int len = s.length();
-    int ret = 0;
-    while ( i >= 0 && j < len ) {
-        if (s.charAt(i) == s.charAt(j)) {
-            ret += (i==j)? 1 : 2;
-            i--;
-            j++;
-        } else {
-            break;
-        }
+    while ( i >= 0 && j < len && s.charAt(i) == s.charAt(j)) {
+        i--;
+        j++;
     }
-    return ret;
+    return j - i - 1;
 }
