7111c786280c3857ff711b4639e46960608b5544
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2020-08-16 17:12:23 -0700

    update

--- Java/6_ZigZagConversion.java
+++ Java/6_ZigZagConversion.java
@@ -1,15 +1,17 @@
-/* Given a string s, find the longest palindromic substring in s. You may assume
-* that the maximum length of s is 1000.
+/* The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+* of rows like this: (you may want to display this pattern in a fixed font for
+* better legibility)
 *
-* Example 1:
+* P   A   H   N
+* A P L S I I G
+* Y   I   R
+* And then read line by line: "PAHNAPLSIIGYIR"
 *
-* Input: "babad"
-* Output: "bab"
-* Note: "aba" is also a valid answer.
-* Example 2:
+* Write the code that will take a string and make this conversion given a number
+* of rows:
 *
-* Input: "cbbd"
-* Output: "bb"
+* string convert(string text, int nRows);
+* convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
 */
 
 public String convert(String s, int numRows) {
