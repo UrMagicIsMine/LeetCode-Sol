dcc19defe1fcb815482d6452abae95eef6347948
UrMagicIsMine <36392090+UrMagicIsMine@users.noreply.github.com>
2020-09-20 16:26:39 -0700

    Create 13_RomantoInteger.java

---
+++ Java/13_RomantoInteger.java
@@ -0,0 +1,79 @@
+/*Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together.
+Twelve is written as, XII, which is simply X + II. The number twenty seven is written
+as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However,
+the numeral for four is not IIII. Instead, the number four is written as IV.
+Because the one is before the five we subtract it making four. The same principle
+applies to the number nine, which is written as IX. There are six instances where
+subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within
+the range from 1 to 3999.
+
+Example 1:
+
+Input: "III"
+Output: 3
+Example 2:
+
+Input: "IV"
+Output: 4
+Example 3:
+
+Input: "IX"
+Output: 9
+Example 4:
+
+Input: "LVIII"
+Output: 58
+Explanation: C = 100, L = 50, XXX = 30 and III = 3.
+Example 5:
+
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+*/
+
+class Solution {
+    public int romanToInt(String s) {
+        int ret = 0;
+        int N = s.length();
+        if (N == 0) {
+            return ret;
+        }
+        HashMap<Character, Integer> RIMaps = new HashMap<Character, Integer>() {{
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }};
+
+        int i = 0;
+        for (; i < N - 1; i++){
+            int cur = RIMaps.get(s.charAt(i));
+            if (cur < RIMaps.get(s.charAt(i+1))) {
+                ret -= cur;
+            } else {
+                ret += cur;
+            }
+        }
+        return ret + RIMaps.get(s.charAt(i));
+    }
+}
